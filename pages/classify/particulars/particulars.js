4986171f14f6155417cbecee5d6142887f416044
davin <davinzhang@outlook.com>
2020-01-26 10:10:57 +0800

    fixed

--- pages/classify/particulars/particulars.js
+++ pages/classify/particulars/particulars.js
@@ -21,7 +21,7 @@ Page({
     sites: '',
     shppingcar: '', //购物车
     attention: '',
-    userId:'',
+    userId: '',
     slideNumber: '1', //详情滑动跳动数字
     collects: false,
     goodsNum: 1,
@@ -39,25 +39,25 @@ Page({
 
   },
   //跳转添加地址页
-  newsite:function(){
+  newsite: function() {
     wx.navigateTo({
       url: '../../address/address',
     })
   },
   //跳转购物车
-  gouwucar: function () {
+  gouwucar: function() {
     wx.switchTab({
       url: '../../shopping/shopping'
     })
   },
   //购物车
-  gouwucar: function () {
+  gouwucar: function() {
     wx.switchTab({
       url: '../../shopping/shopping',
     })
   },
   //关注
-  collect: function () {
+  collect: function() {
     var that = this;
     if (that.data.collects) {
       that.setData({
@@ -149,7 +149,7 @@ Page({
     })
   },
   //分享
-  onShareAppMessage: function (ops) {
+  onShareAppMessage: function(ops) {
     if (ops.from === 'button') {
       // 来自页面内转发按钮
       console.log(ops.target)
@@ -157,11 +157,11 @@ Page({
     return {
       title: '同城优品小程序',
       path: 'pages/classify/particulars/particulars',
-      success: function (res) {
+      success: function(res) {
         // 转发成功
         console.log("转发成功:" + JSON.stringify(res));
       },
-      fail: function (res) {
+      fail: function(res) {
         // 转发失败
         console.log("转发失败:" + JSON.stringify(res));
       }
@@ -169,7 +169,7 @@ Page({
 
   },
   //详情滑动跳动数字
-  current: function (e) {
+  current: function(e) {
     console.log(e)
     var that = this
     that.setData({
@@ -177,7 +177,7 @@ Page({
     })
   },
   // 显示遮罩层
-  showModal: function () {
+  showModal: function() {
     var that = this;
     that.setData({
       hideModal: false
@@ -187,12 +187,12 @@ Page({
       timingFunction: 'ease', //动画的效果 默认值是linear
     })
     this.animation = animation
-    setTimeout(function () {
+    setTimeout(function() {
       that.fadeIn(); //调用显示动画
     }, 200)
   },
   // 隐藏遮罩层
-  hideModal: function () {
+  hideModal: function() {
     var that = this;
     var animation = wx.createAnimation({
       duration: 800, //动画的持续时间 默认400ms   数值越大，动画越慢   数值越小，动画越快
@@ -200,27 +200,27 @@ Page({
     })
     this.animation = animation
     that.fadeDown(); //调用隐藏动画   
-    setTimeout(function () {
+    setTimeout(function() {
       that.setData({
         hideModal: true
       })
     }, 720) //先执行下滑动画，再隐藏模块
   },
   //动画集
-  fadeIn: function () {
+  fadeIn: function() {
     this.animation.translateY(0).step()
     this.setData({
       animationData: this.animation.export() //动画实例的export方法导出动画数据传递给组件的animation属性
     })
   },
-  fadeDown: function () {
+  fadeDown: function() {
     this.animation.translateY(300).step()
     this.setData({
       animationData: this.animation.export(),
     })
   },
   //商品详情价格及内容
-  particulars: function (goodsId) {
+  particulars: function(goodsId) {
     particularsUtil.getProductInfo({
       goodsId,
     }, (res) => {
@@ -237,7 +237,7 @@ Page({
     return particularsUtil.checkUserIslogin(this.data.userId);
   },
   //加入购物车
-  shppingcar: function (e) {
+  shppingcar: function(e) {
     var that = this;
     let isLogin = that.data.isLogin;
     if (isLogin == false) {
@@ -257,7 +257,7 @@ Page({
       wx.request({
         url: app.globalData.urlCart + '/cart/add',
         method: 'Get',
-        success: function (res) {
+        success: function(res) {
           if (res.data.data == '成功加入购物车') {
             wx.showToast({
               title: '添加购物车成功',
@@ -291,20 +291,25 @@ Page({
 
     let goodsid = this.data.selectedGoods;
     console.log(this.data.selectedGoods)
-      let userAddressId = this.data.selectedAddress;
+    let userAddressId = this.data.selectedAddress;
     console.log(this.data.selectedAddress);
-      if (app.globalData.selectedGoods.length > 0) {
-        app.globalData.selectedGoods = [];
-      }
-      app.globalData.selectedGoods.push({ selectedGoods: this.data.selectedGoods, selectedAddress: this.data.selectedAddress });
+    if (app.globalData.selectedGoods.length > 0) {
+      app.globalData.selectedGoods = [];
+    }
+    app.globalData.selectedGoods.push({
+      selectedGoods: this.data.selectedGoods,
+      selectedAddress: this.data.selectedAddress
+    });
 
     if (JSON.stringify(this.data.selectedGoods) == '{}' || JSON.stringify(this.data.selectedAddress) == '{}') {
       wx.showToast({
         title: '规格或地址未选',
         icon: 'none',
       });
-    } else if (JSON.stringify(this.data.selectedGoods) !== '{}' && JSON.stringify(this.data.selectedAddress) !== '{}' ) {
-      wx.navigateTo({ url: `../../ljbuy/ljbuy?purchasePrice=` });
+    } else if (JSON.stringify(this.data.selectedGoods) !== '{}' && JSON.stringify(this.data.selectedAddress) !== '{}') {
+      wx.navigateTo({
+        url: `../../ljbuy/ljbuy?purchasePrice=`
+      });
     }
     //  
   },
@@ -313,14 +318,14 @@ Page({
     var that = this;
     wx.getStorage({
       key: 'user',
-      success: function (res) {
+      success: function(res) {
         that.setData({
           canGetSite: true,
           isUserId: true,
           userId: res.data.userId
         })
       },
-      fail: function (res) {
+      fail: function(res) {
         that.setData({
           canGetSite: false,
           isUserId: false
@@ -334,14 +339,14 @@ Page({
     var that = this;
     wx.getStorage({
       key: 'user',
-      success: function (res) {
+      success: function(res) {
 
         that.setData({
           userId: res.data.userId,
         })
 
       },
-      fail: function (res) {
+      fail: function(res) {
         that.setData({
           show: true,
           isLogin: false
@@ -354,25 +359,25 @@ Page({
     var that = this;
     wx.getStorage({
       key: 'user',
-      success: function (res) {
+      success: function(res) {
         that.setData({
           userId: res.data.userId,
           isLogin: true
         })
       },
-      fail: function (res) {
+      fail: function(res) {
         that.setData({
           isLogin: false
         })
       }
     })
   },
-  nologin: function () {
+  nologin: function() {
     this.setData({
       show: false
     })
   },
-  getUserInfo: function (e) {
+  getUserInfo: function(e) {
     let main = this;
     wx.showLoading({
       title: '正在登录',
@@ -402,7 +407,7 @@ Page({
                 data: res.userInfo
               })
               wx.login({
-                success: function (res) {
+                success: function(res) {
                   console.log(res);
                   wx.request({
                     url: app.globalData.urlLogin + '/user/wxLogin',
@@ -418,7 +423,7 @@ Page({
                       signature: main.data.global.signature
                     },
 
-                    success: function (res) {
+                    success: function(res) {
                       console.log("登录", res);
                       if (res.data.status == 200) {
                         wx.hideLoading();
@@ -428,7 +433,7 @@ Page({
                         wx.setStorage({
                           key: 'user',
                           data: res.data.data,
-                          success: function (res) {
+                          success: function(res) {
                             wx.navigateTo({
                               url: '../register/register?userId=' + main.data.userId,
                             })
@@ -443,7 +448,7 @@ Page({
                         })
                       }
                     },
-                    fail: function (res) {
+                    fail: function(res) {
                       wx.hideLoading();
                       main.setData({
                         show: true,
@@ -481,8 +486,11 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    const { id, price } = options;
+  onLoad: function(options) {
+    const {
+      id,
+      price
+    } = options;
     var that = this;
     this.isUserId();
     this.firstIsLogin();
@@ -494,7 +502,7 @@ Page({
     //用户的id
     wx.getStorage({
       key: 'user',
-      success: function (res) {
+      success: function(res) {
         that.setData({
           userId: res.data.userId,
         })
